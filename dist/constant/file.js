48f0a211c128d81e9693271c0091e1c767a187e0
hamanhnguyen <manhhaql@gmail.com>
2020-11-16 15:01:08 +0700

    remove dist

--- dist/constant/file.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var FILE_TYPE_IMAGE = exports.FILE_TYPE_IMAGE = 'image';
-var FILE_TYPE_UNKNOWN = exports.FILE_TYPE_UNKNOWN = 'unknown';
