3ad1c788e1c57395970495eedc01cb1028fcc429
hamanhnguyen <manhhaql@gmail.com>
2020-11-17 15:48:54 +0700

    test postinstall

--- src/core/cloud_storage/index.js
+++ src/core/cloud_storage/index.js
@@ -6,7 +6,7 @@ import config from '../../config';
 
 export const storage = new Storage({
     projectId: process.env.FIREBASE_PROJECT_ID  || config.firebase.projectId,
-    keyFilename: path.join(__dirname, process.env.GOOGLE_CREDENTIALS || '../../../ServiceAccountKey.json')
+    keyFilename: path.join(__dirname, '../../../ServiceAccountKey.json')
 });
 
 export const bucket = storage.bucket(process.env.FIREBASE_STORAGE_BUCKET || config.firebase.storageBucket);
