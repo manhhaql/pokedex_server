d93210e0119ff6c2b4a24d73ed5741b4843c45d5
hamanhnguyen <manhhaql@gmail.com>
2020-11-17 13:17:26 +0700

    nomessage

--- src/core/cloud_storage/index.js
+++ src/core/cloud_storage/index.js
@@ -5,8 +5,8 @@ import path from 'path';
 import config from '../../config';
 
 export const storage = new Storage({
-    projectId: config.firebase.projectId,
+    projectId: process.env.FIREBASE_PROJECT_ID || config.firebase.projectId,
     keyFilename: path.join(__dirname, process.env.GOOGLE_APPLICATION_CREDENTIALS || '../../../ServiceAccountKey.json')
 });
 
-export const bucket = storage.bucket(config.firebase.storageBucket);
+export const bucket = storage.bucket(process.env.FIREBASE_STORAGE_BUCKET || config.firebase.storageBucket);
