f37be874d391c54f5a9cdfab4f8ae07997f0e995
hamanhnguyen <manhhaql@gmail.com>
2020-11-17 13:29:37 +0700

    clear git cache

--- src/core/cloud_storage/index.js
+++ src/core/cloud_storage/index.js
@@ -5,7 +5,7 @@ import path from 'path';
 import config from '../../config';
 
 export const storage = new Storage({
-    projectId: process.env.FIREBASE_PROJECT_ID || config.firebase.projectId,
+    projectId: process.env.FIREBASE_PROJECT_ID  || config.firebase.projectId,
     keyFilename: path.join(__dirname, process.env.GOOGLE_APPLICATION_CREDENTIALS || '../../../ServiceAccountKey.json')
 });
 
